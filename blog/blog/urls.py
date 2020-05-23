f1c1925e69c3fdc5263d1d1db4f65a51927b8c9e
FelipeSavaris <fesavaris@gmail.com>
2020-05-23 20:18:49 -0300

    trabalhando com templates e injetando informações

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world
 # deve importar toda view criada
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
+    path('blog', include('website.urls')),
 ]
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
 
-# Create your views here.
+
+def hello_blog(request):
+    lista = [
+        'Django', 'Python', 'Git', 'Htnl', 'Banco de Dados', 'Linux', 'Nginx',
+        'Uwsgi', 'Systemctl'
+    ]
+    data = {'name': 'Curso Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
+# o render carrega um template, diferente do httpreponse que estava estático
