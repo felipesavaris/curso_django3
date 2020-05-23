79e014c4185ed73c91fcb0d1304891fa38fe4974
FelipeSavaris <fesavaris@gmail.com>
2020-05-23 20:53:17 -0300

    trabalhando com dados do banco

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from .models import Post
 
 
 def hello_blog(request):
@@ -7,6 +8,13 @@ def hello_blog(request):
         'Django', 'Python', 'Git', 'Htnl', 'Banco de Dados', 'Linux', 'Nginx',
         'Uwsgi', 'Systemctl'
     ]
-    data = {'name': 'Curso Django 3', 'lista_tecnologias': lista}
+    list_posts = Post.objects.all()  # ou Post.objects.filter()
+
+    data = {
+        'name': 'Curso Django 3',
+        'lista_tecnologias': lista,
+        'posts': list_posts
+    }
+
     return render(request, 'index.html', data)
 # o render carrega um template, diferente do httpreponse que estava estático
