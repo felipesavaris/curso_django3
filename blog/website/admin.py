b0a6b5f889ebb158926aaa4bdf23d7f6b423e8ca
FelipeSavaris <fesavaris@gmail.com>
2020-05-23 22:47:00 -0300

    utilizando TextChoices - Django3

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
+
+#cria uma melhor visualizaçao dos registros no admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'full_name', 'categories']
+    # cria as visualizaões do dados cadastrados
+
+    search_fields = ['title', 'sub_title'] # add item de pesquisa - lupa
+    #existe também o filter_horizontal para casos de muitos pra muitos
+
+    #fields = ['title']
+    # este caso não apareceria mais o sub_title no admin
+
+
+admin.site.register(Post, PostAdmin)
 #isto add a model ao admin
-admin.site.register(Post)
--- blog/website/models.py
+++ blog/website/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 
 
+#utilizando TextChoices
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     conotent = models.TextField()
+    #adicionando o textchoices
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
 
+    def __str__(self):
+        return self.title
+    #método para mostrar o titulo quando for selecionado no admin
+
+    def full_name(self):
+        return self.title + self.sub_title
+        # adicionar o full_name no admin para visualizaçao
+
+    full_name.admin_order_field = 'title'
