977cebf837ba6ec784427f99adeba23eb8eb799b
FelipeSavaris <fesavaris@gmail.com>
2020-06-10 21:21:40 -0300

    atualiacao models

--- blog/website/models.py
+++ blog/website/models.py
@@ -20,13 +20,16 @@ class Post(models.Model):
     )
     approved = models.BooleanField(default=True)
 
+
     def __str__(self):
         return self.title
     #método para mostrar o titulo quando for selecionado no admin
 
+
     def full_name(self):
         return self.title + self.sub_title
         # adicionar o full_name no admin para visualizaçao
+
 
     def get_category_label(self):
         return self.get_categories_display()
